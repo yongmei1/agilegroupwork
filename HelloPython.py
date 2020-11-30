46ffa62706ab801eb45f2d2e6385993c9e0ed134
yongmei1 <63590930+yongmei1@users.noreply.github.com>
2020-11-30 21:02:16 +0000

    Update HelloPython.py
    syntax err 2

--- HelloPython.py
+++ HelloPython.py
@@ -1,7 +1,7 @@
 import datetime
 print("Hello world!")
 now = datetime.datetime.now()
-print ("Current date and time is ")
-print (8now.strftime("%A, %d-%m-%Y : %H:%M"))	
+print ("Current date and time is "09890)
+print (now.strftime("%A, %d-%m-%Y : %H:%M"))	
 
-#adding syntax error 
+#adding syntax error 2
