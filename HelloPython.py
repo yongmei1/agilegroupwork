357a980701af223e68b0e609404171671ca413a5
yongmei1 <63590930+yongmei1@users.noreply.github.com>
2020-12-02 22:28:45 +0000

    Update HelloPython.py
    commit number 2

--- HelloPython.py
+++ HelloPython.py
@@ -1,6 +1,6 @@
 import datetime
 print("Hello world!")
-printf("agile processes is the best")
+print("agile processes is the best")
 now = datetime.datetime.now()
 print ("Current date and time is ")
 print (now.strftime("%A, %d-%m-%Y : %H:%M"))	
