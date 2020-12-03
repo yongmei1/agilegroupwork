72355af89460634478bf7ddcb2a836043a2c26d2
yongmei1 <63590930+yongmei1@users.noreply.github.com>
2020-12-03 13:18:22 +0000

    Update HelloPython.py
    change from 10 to 21

--- HelloPython.py
+++ HelloPython.py
@@ -2,8 +2,9 @@ import datetime
 print("Hello world!")
 print("agile processes is the best")
 print("adding another test line")
-x = 10
+x = 21
 print("x value = ", x )
+
 now = datetime.datetime.now()
 print ("Current date and time is ")
 print (now.strftime("%A, %d-%m-%Y : %H:%M"))	
